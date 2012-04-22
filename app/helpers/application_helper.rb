0acfcef4800ed8244ca4211029a85283725ea55d
Vitaliy Beskrovny <bva@aejis.eu>
2012-04-23 02:00:50 +0300

    + show_flash helper is added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def show_flash
+    [:notice, :alert, :error].map { |type|
+      content_tag(:div, flash[type], class: type) if flash[type]
+    }.join.html_safe
+  end
 end
