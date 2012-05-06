16c1be26266e4b1c77a508a91041dd5f9210e472
Vitaliy Beskrovny <bva@aejis.eu>
2012-05-06 22:42:03 +0300

    + associations tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Event do
 
+  it { should have_one(:game) }
+
+  it { should belong_to(:user) }
+
   it { should have_db_column(:name).of_type(:string) }
   it { should have_db_column(:desc).of_type(:string) }
   it { should have_db_column(:type).of_type(:string) }
@@ -10,6 +14,5 @@ describe Event do
   it { should have_db_column(:author_id).of_type(:integer) }
   it { should have_db_column(:created_at).of_type(:datetime) }
   it { should have_db_column(:updated_at).of_type(:datetime) }
-
 
 end
