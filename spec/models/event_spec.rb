f0b17b0a3f68fb3b170e7bd1d37767396d625a12
Vitaliy Beskrovny <bva@aejis.eu>
2012-05-06 22:19:58 +0300

    + event tests

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Event do
+
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:desc).of_type(:string) }
+  it { should have_db_column(:type).of_type(:string) }
+  it { should have_db_column(:start_date).of_type(:datetime) }
+  it { should have_db_column(:finish_date).of_type(:datetime) }
+  it { should have_db_column(:author_id).of_type(:integer) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
+
+
+end
