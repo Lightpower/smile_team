97b074c9ec8535191a29c835bd9edbf77e6bbcdf
Vitaliy Beskrovny <bva@aejis.eu>
2012-05-08 01:24:30 +0300

    + integration test template

---
+++ spec/integration/event_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature "Home" do
+  background do
+    visit root_path
+  end
+
+
+  pending it "should do something" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == true
+  end
+end
