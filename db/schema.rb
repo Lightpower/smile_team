3f77155338e12c2043ea340f9e6e5c77cc3df065
Vitaliy Beskrovny <bva@aejis.eu>
2012-05-08 04:48:53 +0300

    + migration

--- db/schema.rb
+++ db/schema.rb
@@ -60,9 +60,9 @@ ActiveRecord::Schema.define(:version => 201205040003400) do
 
   create_table "project_formats", :force => true do |t|
     t.string   "name"
-    t.integer  "project_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id", :null => false
   end
 
   add_index "project_formats", ["name"], :name => "index_project_formats_on_name", :unique => true
@@ -85,8 +85,8 @@ ActiveRecord::Schema.define(:version => 201205040003400) do
     t.integer  "scores"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",   :null => false
+    t.string   "encrypted_password",     :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(:version => 201205040003400) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "login"
+    t.boolean  "is_enabled",             :default => true, :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
