dc47ca80c125b44d6de32e2a0845fc5e71389c46
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-24 17:21:31 +0300

    + validate event and project_format; + project method

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,11 @@ class Game < ActiveRecord::Base
   belongs_to :event
   belongs_to :project_format, foreign_key: :format_id
 
+  validates_presence_of :event
+  validates_presence_of :project_format
+
+  def project
+    self.project_format.project
+  end
+
 end
