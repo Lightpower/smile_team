d7b509fc8d2dd5a6cdc9adaed4eac48d3442dd26
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-24 14:40:29 +0300

    + load_and_authorize_resource :user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource :user
+
   # GET /users
   # GET /users.json
   def index
@@ -13,8 +15,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
-    @user = User.new
+    #@user = User.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -34,13 +34,12 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
   end
 
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params[:user])
+    #@user = User.new(params[:user])
 
     respond_to do |format|
       if @user.save
@@ -56,8 +55,6 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -72,7 +69,6 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
 
     respond_to do |format|
