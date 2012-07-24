7c9ede6f86933381a99c86bc12e953408da9aa92
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-24 14:39:12 +0300

    + accepts_nested_attributes_for :game

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,21 @@ class Event < ActiveRecord::Base
   has_one :game
   belongs_to :author, foreign_key: :author_id, class_name: "User"
 
+  accepts_nested_attributes_for :game
+
   class << self
     def in_future
       self.where("start_date >= ?", Date.today)
     end
 
+  end
+
+  def type
+    if self.game.present?
+      "Game"
+    else
+      "Unknown"
+    end
   end
 
 
