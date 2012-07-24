47820915efcab10312f90a50786a5500ad424a17
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-24 17:21:08 +0300

    + validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ActiveRecord::Base
   has_one :game
   belongs_to :author, foreign_key: :author_id, class_name: "User"
 
+  validates_presence_of :author
+
   accepts_nested_attributes_for :game
 
   class << self
