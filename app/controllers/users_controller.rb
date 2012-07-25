d2943fab4a06d4985deb696358d1c01368ae586c
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-25 19:10:27 +0300

    *

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,6 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
-    #@user = User.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user }
@@ -39,8 +37,6 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    #@user = User.new(params[:user])
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
