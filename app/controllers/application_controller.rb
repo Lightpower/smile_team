faba13097d3d26ec1aa3493e7d8df9fc1c8e0d57
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-27 17:39:36 +0300

    + redirecting after signing in, signing out and AccessDenied exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if can? :read, Event
+      events_path
+    elsif current_user.present? && (can? :read, current_user)
+      user_path(current_user)
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
 end
