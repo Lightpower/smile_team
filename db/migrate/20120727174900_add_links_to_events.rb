c8234a7712b688990127806f2967ae86fafd1490
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-27 17:59:32 +0300

    * event => game

--- db/migrate/20120727174900_add_links_to_events.rb
+++ db/migrate/20120727174900_add_links_to_events.rb
@@ -1,6 +1,6 @@
 class AddLinksToEvents < ActiveRecord::Migration
   def change
-    change_table(:events) do |t|
+    change_table(:games) do |t|
       t.string :link_to_site
       t.string :link_to_forum
     end
