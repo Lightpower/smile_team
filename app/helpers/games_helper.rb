60e9a5605d705b8c03678777dac832b8020c6761
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-28 17:40:34 +0300

    + game_links helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,10 @@
+# coding: utf-8
 module GamesHelper
+  def game_links(game)
+    result = ""
+    result << link_to("Перейти к анонсу на сайте", game.link_to_site) if game.link_to_site.present?
+    result << " | " if game.link_to_site.present? && game.link_to_forum.present?
+    result << link_to("Перейти к форуму", game.link_to_forum) if game.link_to_forum.present?
+    content_tag(:p, result.html_safe).html_safe
+  end
 end
