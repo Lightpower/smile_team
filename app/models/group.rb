6337a52fa3a994927cdb8658fe0bb0cf4bb90fa8
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-28 05:24:34 +0300

    + name is uniq; + approve_all; + reject_all

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,20 @@ class Group < ActiveRecord::Base
 
   has_many :users
 
+  validates_uniqueness_of :name
+
   def captain
     self.users.where(site_role: "captain").first
+  end
+
+  def approve_all
+    connection = ActiveRecord::Base.connection()
+    connection.execute("update users set site_role='mate' where group_id=#{ActiveRecord::Base.sanitize(self.id)} and (site_role='guest' or site_role is null)")
+  end
+
+  def reject_all
+    connection = ActiveRecord::Base.connection()
+    connection.execute("update users set group_id=null where group_id=#{ActiveRecord::Base.sanitize(self.id)} and (site_role='guest' or site_role is null)")
   end
 
 end
