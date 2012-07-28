41a65c2ed321b72f5567db9dfdd279d57c670218
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-28 05:21:42 +0300

    + group controller

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,28 @@
+class GroupsController < ApplicationController
+  before_filter :authorize_join_requests
+
+
+  def join_requests
+    @users = User.unapproved(current_user.group)
+    debugger
+    1+1
+  end
+
+  def approve_all
+    res = current_user.group.approve_all
+    puts res
+    render :join_requests
+  end
+
+  def reject_all
+    res = current_user.group.reject_all
+    puts res
+    render :join_requests
+  end
+
+private
+
+  def authorize_join_requests
+    authorize! :manage, :join_requests
+  end
+end
