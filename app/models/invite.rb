63f070f76c3acaea6d81f5919afda84e1d89e7aa
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-30 16:46:33 +0300

    + STATES; + accept, reject, pend methods

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,4 +3,32 @@ class Invite < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
 
+  ##
+  #  Possible values for invite state
+  #
+  STATES = {sent: "sent", read: "read", pending: "pending", rejected: "rejected", accepted: "accepted"}
+
+  ##
+  # Accepting current invite
+  #
+  def accept
+    self.state = STATES[:accepted]
+    self.save
+  end
+
+  ##
+  # Rejecting current invite
+  #
+  def reject
+    self.state = STATES[:rejected]
+    self.save
+  end
+
+  ##
+  # Pending current invite
+  #
+  def pend
+    self.state = STATES[:pending]
+    self.save
+  end
 end
