9f18b8278c349ef539ed0dad54719bef907db38a
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-30 19:52:29 +0300

    * edit_state_icons: login is added

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -23,8 +23,12 @@ module InvitesHelper
   ##
   # Show icons for invite state managing
   #
-  def edit_state_icons(invite)
+  def edit_state_icons(invite, add_login=false)
     return_string = ""
+    if add_login
+      return_string += content_tag(:b, invite.user.login).html_safe
+      return_string += " " if return_string.present?
+    end
     { :accepted => "+1", :rejected => "-1", :pending => "?" }.each do |elem|
       return_string += " " if return_string.present?
       link_string = link_to(elem[1], class: "#{Invite::STATES[elem[0]]}_invite").html_safe
@@ -33,5 +37,17 @@ module InvitesHelper
     end
 
     content_tag(:div, return_string.html_safe, { "data-id" => invite.id } ).html_safe
+  end
+
+  ##
+  # Choose the invite of defined user from invite list
+  #
+  def show_my_invite(user, invites)
+    invite = invites.where(user_id: user.id).first
+    return_string = ""
+    if invite.present?
+      return_string += edit_state_icons(invite, true)
+    end
+    return_string.html_safe
   end
 end
