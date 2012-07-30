7bc4c775ffed653573a8e4c150f75f8c96d1da44
Vitaliy Beskrovny <bva@aejis.eu>
2012-07-30 16:46:05 +0300

    + comments; + group method to access event's group; + invite, invite_all methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,10 +8,35 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :game
 
   class << self
+    ##
+    # Scope of events which start time is in future
+    #
     def in_future
       self.where("start_date >= ?", Date.today)
     end
 
   end
+
+  ##
+  # Group of users which this event created for
+  #
+  def group
+    self.author.group
+  end
+
+  ##
+  # Create invite on current event for defined user
+  #
+  def invite(user)
+    Invite.create(user: user, event: self, state: Invite::STATES[:sent]) if Invite.where(user: user, event: self).blank?
+  end
+
+  ##
+  # Create invites on current event for all users in group
+  #
+  def invite_all
+    self.group.users.each { |user| self.invite(user) }
+  end
+
 
 end
